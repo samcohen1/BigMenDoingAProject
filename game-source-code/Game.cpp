e61f4ef7b4b67e0361e44a7e17fff89e5bd15b93
Yishai Basserabie <yish@elucidate.ai>
2023-09-05 11:44:52 +0200

    Added Game .cpp and Game.h

---
+++ game-source-code/Game.cpp
@@ -0,0 +1,40 @@
+#include "Game.h"
+
+Game::Game() {
+    this->_init_window();
+    this->_init_background();
+}
+
+void Game::_init_window() {
+    this->window_ = std::make_shared<sf::RenderWindow>(sf::VideoMode(this->game_width_, this->game_height_, 32), "Byte Defenders", sf::Style::Titlebar | sf::Style::Close);
+}
+
+void Game::_init_background() {
+    if(!(this->background_texture_.loadFromFile("resources/Background.png"))) return;
+    this->background_sprite_.setTexture(this->background_texture_);
+    this->background_sprite_.setPosition(-2.f*this->game_width_, 0.f);
+    this->background_sprite_.setScale(this->x_scale_, this->y_scale_);
+}
+
+void Game::update() {
+    sf::Event event;
+    while (this->window_->pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            this->window_->close();
+        }
+    }
+}
+
+void Game::render() {
+    this->window_->clear();
+    this->window_->draw(this->background_sprite_);
+
+    this->window_->display();
+}
+
+void Game::run() {
+    while(this->window_->isOpen()) {
+        this->update();
+        this->render();
+    }
+}
---
+++ game-source-code/Game.h
@@ -0,0 +1,33 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics.hpp>
+
+class Game {
+    private:
+        std::shared_ptr<sf::RenderWindow> window_;
+        sf::Texture background_texture_;
+        sf::Sprite background_sprite_;
+
+        // CONSTANTS
+        const float original_background_width_ = 1920;
+        const float original_background_height_ = 1080;
+        const float game_width_ = 1400;
+        const float game_height_ = 800;
+        const float x_scale_ = this->game_width_/this->original_background_width_;
+        const float y_scale_ = this->game_height_/this->original_background_height_;
+
+        void _init_window();
+        void _init_background();
+        void update();
+        void render();
+
+    public:
+        Game();
+        
+        void run();
+        
+};
+
+
+#endif
