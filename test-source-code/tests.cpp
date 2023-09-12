34238cfe0b56e1fa85533778f98af87e625d4a8f
Samuel Cohen <sam.cohen2001@gmail.com>
2023-09-12 09:49:35 +0200

    Includes

--- test-source-code/tests.cpp
+++ test-source-code/tests.cpp
@@ -1,6 +1,15 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-
+#include "Game.h"
+#include "Bullet.h"
+#include "Splash_Screen.h"
+#include "About_Screen.h"
+#include "How_To_Play_Screen.h"
+#include "Player.h"
+#include "Professor.h"
+#include "Professor_Assignment.h"
+#include "Enemy.h"
+#include "Throwable.h"
 
 
 // TEST_CASE("testing the left paddle")
